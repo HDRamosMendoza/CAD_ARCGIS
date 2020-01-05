6ac5f52b09ffcd3b5c70405633135926229a3269
Daniel Ramos Mendoza <heber.daniel.ramos.mendoza@gmail.com>
2020-01-04 19:50:54 -0500

    Se agrego la conversión de CAD a GDB pero se tiene el problema de los nombre y que la anotaciones se guarda en otra subconjunto.

--- main.py
+++ main.py
@@ -1,66 +1,80 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import os
-import csv
-from openpyxl import Workbook
 from os import listdir
-from os.path import isfile, join
+import arcpy
 
-count = 0
-pathWord = r'D:\RepositorioGitLab\Frontend\Electron\data_source\ASCII'
-pathCSV = r'D:\RepositorioGitLab\Frontend\Electron\xlsx'
+pathDWG = r'D:\RepositorioGitHub\CAD_ARCGIS\DWG'
+prj = r'D:\RepositorioGitHub\CAD_ARCGIS/PRJ\WGS84_18S.prj'
 
 # Lista los archivos de la carpeta
 def dataSource(ruta = '.'):
     return listdir(ruta)
 
-# Valida si es número o no
-def isNumber(param_in):
-    try:
-        float(param_in)
-        return True
-    except ValueError:
-        return False
-
+'''
 # Eliminar archivos de procesamiento.
 def deleteFiles(folder):
 	files_dump = [join(folder, c) for c in listdir(folder)]
 	files_dump = filter(lambda c: isfile(c), files_dump)
 	[os.remove(c) for c in files_dump]
+'''
 
-# Configurando EXCEL
-wb = Workbook()
-ws = wb.active
-ws.sheet_properties.tabColor = "1072BA"
+arcpy.env.workspace = "D:\RepositorioGitHub\CAD_ARCGIS"
+arcpy.env.overwriteOutput = True
 
+count = 0
+#input_cad = r"D:\RepositorioGitHub\CAD_ARCGIS\DWG\3101009011.dwg"
+out_geodatabase = "Demo.gdb"
+
+
+scale = "1000"
+
+# Execute CreateFeaturedataset
+#arcpy.CadToGeodatabase_conversion(input_cad_dataset, out_gdb_path, out_dataset_name, reference_scale)
+#arcpy.CreateFileGDB_management("C:/output", "HabitatAnalysis.gdb")
+'''
+arcpy.CADToGeodatabase_conversion(
+    input_cad,
+    out_geodatabase,
+    out_dataSet,
+    scale,
+    prj
+)
+'''
 # Antes de entrar a la ruta se tiene que verificar si existe
-if (os.path.exists(pathWord)):
-	deleteFiles(pathCSV)
-	outDir = dataSource(pathWord)
-	for infile in outDir:
-		inPath = os.path.join(pathWord, infile)
-		print inPath
+if (os.path.exists(pathDWG)):
+    # deleteFiles(pathCSV)
+    outDir = dataSource(pathDWG)
+    for infile in outDir:
+        count = count + 1
+        # path DWG
+        input_cad = os.path.join(pathDWG, infile)
+        desc = arcpy.Describe(input_cad)
+        #print("Name: {}".format(desc.name))
+        #print("File {}".format(desc.file))
+        #print("Extension {}".format(desc.extension))
+        nameDataSet_CAD = "MSI_"+ desc.name.replace("."+ desc.extension, "").replace(" ", "").replace("-", "")
+        #for child in desc.children:
+        #print "\t%s = %s" % (child.name, child.dataType)
+        print(nameDataSet_CAD)
+        out_dataSet = nameDataSet_CAD
+        arcpy.CADToGeodatabase_conversion(
+            os.path.abspath(input_cad),
+            out_geodatabase,
+            out_dataSet,
+            scale,
+            prj
+        )
 
-		# Validamos la existencia si el archivo tiene alguna información
-		if os.stat(inPath).st_size == 0 :
-			print "Archivo vacio: {0}".format(infile)
-		else:
-			count = count + 1			
-			with open(inPath,'r') as file:
-				for line in file:
-					lineArr = line.split()
-					ws.title = infile
-					if isNumber(lineArr[0]) and isNumber(lineArr[1]):
-						ws.append([lineArr[0], lineArr[1]])
-					else:
-						ws.append(['X', 'Y'])
-			file.close()
-			wb.save(os.path.join(pathCSV, "{0}.xlsx".format(infile)))
-			
-		# Porcentaje de archivos procesados
-		percentage = (count*100)/len(outDir)
-		print "{0}%".format(percentage)
-	# fileTxt = ls(pathWord)
-	# print pathWord+""+fileTxt
+        # Validamos la existencia si el archivo tiene alguna información
+        #if os.stat(inPath).st_size == 0:
+        #print("Archivo vacio: {0}".format(infile))
+
+        # Porcentaje de archivos procesados
+        percentage = (count*100)/len(outDir)
+        print("{0}%".format(percentage))
+
+    # fileTxt = ls(pathWord)
+    # print pathWord+""+fileTxt
 else:
-	print "No existe RUTA"
+    print("No existe RUTA")
